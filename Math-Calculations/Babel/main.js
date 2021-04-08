1649f734b5f2a5399583c368b51b939ee7387d01
Mariam Gevorgyan <mariam.gevorgyan8008@gmail.com>
2021-04-08 22:17:08 +0400

    my frist homework

---
+++ Math-Calculations/Babel/main.js
@@ -0,0 +1,7 @@
+import {circleArea,hypotenuse,pow} from './math.js';
+import {format} from './format.js';
+console.log(format(8,circleArea(8)));
+console.log(hypotenuse(5,11));
+console.log(pow(5,9));
+
+
