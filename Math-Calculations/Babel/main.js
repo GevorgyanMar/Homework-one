2841c40abd97b3122750456aab461b95a084a73d
Mariam Gevorgyan <mariam.gevorgyan8008@gmail.com>
2021-04-08 23:54:49 +0400

    fixed bug

--- Math-Calculations/Babel/main.js
+++ Math-Calculations/Babel/main.js
@@ -1,7 +1,7 @@
 import {circleArea,hypotenuse,pow} from './math.js';
 import {format} from './format.js';
-console.log(format(8,circleArea(8)));
-console.log(hypotenuse(5,11));
-console.log(pow(5,9));
+console.log(format("the area of a circle with a radius of 8", circleArea(8)));
+console.log(format("the hypotenuse of a triangle with sides 8 and 11", hypotenuse(5, 11).hypotenuse));
+console.log(format("5 to the power of 9", pow(5, 9)));
 
 
